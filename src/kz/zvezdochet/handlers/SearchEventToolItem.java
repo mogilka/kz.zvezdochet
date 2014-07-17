38330c139e5d02368908c4c4f5b3943ad69244b3
nataly <info@zvezdochet.kz>
2014-07-17 14:49:12 +0600

    Добавлен виджет поиска (эксперимент)

---
+++ src/kz/zvezdochet/handlers/SearchEventToolItem.java
@@ -0,0 +1,25 @@
+package kz.zvezdochet.handlers;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * Поле поиска события
+ * @author Nataly Didenko
+ *
+ */
+public class SearchEventToolItem {
+	@PostConstruct
+	public void createControls(Composite parent) {
+		final Composite comp = new Composite(parent, SWT.NONE);
+		comp.setLayout(new GridLayout());
+		Text text = new Text(comp, SWT.SEARCH | SWT.ICON_SEARCH | SWT.CANCEL | SWT.BORDER);
+		text.setMessage(Messages.SearchEventToolItem_Search);
+		GridDataFactory.fillDefaults().hint(130, SWT.DEFAULT).applyTo(text);
+	}
+}
