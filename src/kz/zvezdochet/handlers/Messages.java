61bbb007446ac16f0e0ad2cf796bb9d099a47459
nataly <info@zvezdochet.kz>
2014-07-17 14:50:49 +0600

    Локализация

---
+++ src/kz/zvezdochet/handlers/Messages.java
@@ -0,0 +1,15 @@
+package kz.zvezdochet.handlers;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "kz.zvezdochet.handlers.messages"; //$NON-NLS-1$
+	public static String SearchEventToolItem_Search;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
