bb720329852578a1ffd31197436a7646205d3f27
nataly <info@zvezdochet.kz>
2014-09-06 21:59:33 +0600

    Сохраняем историю просмотра событий

---
+++ src/kz/zvezdochet/handler/RecentEventsHandler.java
@@ -0,0 +1,59 @@
+package kz.zvezdochet.handler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import kz.zvezdochet.bean.Event;
+import kz.zvezdochet.core.handler.Handler;
+import kz.zvezdochet.core.ui.util.DialogUtil;
+import kz.zvezdochet.part.SearchPart;
+import kz.zvezdochet.service.EventService;
+import kz.zvezdochet.util.Constants;
+
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
+import org.osgi.service.prefs.Preferences;
+
+/**
+ * Поиск списка недавно просматриваемых событий
+ * @author Nataly Didenko
+ *
+ */
+public class RecentEventsHandler extends Handler {
+	@Inject
+	private EPartService partService;
+
+	@Execute
+	public void execute() {
+		try {
+			updateStatus("Поиск", false);
+			Preferences preferences = InstanceScope.INSTANCE.getNode("kz.zvezdochet");
+			Preferences recent = preferences.node(Constants.PREF_RECENT);
+			String eventids = recent.get(Constants.PREF_RECENT_EVENT, "");
+			if (eventids.length() > 0) {
+				String[] ids = eventids.split(",");
+				List<Event> events = new ArrayList<Event>();
+				EventService service = new EventService();
+				for (String string : ids) {
+					Event event = (Event)service.find(Long.valueOf(string));
+					events.add(event);
+				}
+				MPart part = partService.findPart("kz.zvezdochet.part.events");
+			    part.setVisible(true);
+			    partService.showPart(part, PartState.VISIBLE);
+			    SearchPart searchPart = (SearchPart)part.getObject();
+			    searchPart.setData(events);
+				updateStatus("Поиск завершён", false);
+			} else
+				DialogUtil.alertInfo("История пуста");
+		} catch (Exception e) {
+			DialogUtil.alertError(e.getMessage());
+			e.printStackTrace();
+		}
+	}		
+}
---
+++ src/kz/zvezdochet/util/Constants.java
@@ -0,0 +1,12 @@
+package kz.zvezdochet.util;
+
+/**
+ * Общеупотребительные значения
+ * @author Nataly Didenko
+ *
+ */
+public class Constants {
+	public static final String PREF_RECENT = "recent";
+	public static final String PREF_RECENT_EVENT = "recent_event";
+	public static final int PREF_RECENT_MAX = 30;
+}
