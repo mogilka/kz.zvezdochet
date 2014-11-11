8104f4620e76a15c0c865b0342877c9a29aa943d
nataly <info@zvezdochet.kz>
2014-11-11 11:26:08 +0600

    Добавлен темперамент

--- src/kz/zvezdochet/bean/Element.java
+++ src/kz/zvezdochet/bean/Element.java
@@ -8,4 +8,17 @@ import kz.zvezdochet.core.bean.DiagramDictionary;
  */
 public class Element extends DiagramDictionary {
 	private static final long serialVersionUID = 2457703926076101583L;
+	
+	/**
+	 * Темперамент
+	 */
+	private String temperament;
+
+	public String getTemperament() {
+		return temperament;
+	}
+
+	public void setTemperament(String temperament) {
+		this.temperament = temperament;
+	}
 }
