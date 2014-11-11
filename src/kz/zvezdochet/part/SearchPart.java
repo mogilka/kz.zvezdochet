38580c60b241a4e1b3d0ec81403bdbe632dc2c0b
nataly <info@zvezdochet.kz>
2014-11-11 11:33:02 +0600

    Поиск по знаку и дому

--- src/kz/zvezdochet/part/SearchPart.java
+++ src/kz/zvezdochet/part/SearchPart.java
@@ -3,25 +3,40 @@ package kz.zvezdochet.part;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import kz.zvezdochet.bean.Aspect;
+import kz.zvezdochet.bean.House;
+import kz.zvezdochet.bean.Planet;
+import kz.zvezdochet.bean.Sign;
 import kz.zvezdochet.core.service.DataAccessException;
 import kz.zvezdochet.core.ui.Tab;
+import kz.zvezdochet.core.ui.provider.DictionaryLabelProvider;
 import kz.zvezdochet.core.ui.view.ModelLabelProvider;
 import kz.zvezdochet.core.ui.view.ModelListView;
 import kz.zvezdochet.core.ui.view.View;
 import kz.zvezdochet.core.util.DateUtil;
 import kz.zvezdochet.service.EventService;
+import kz.zvezdochet.service.HouseService;
+import kz.zvezdochet.service.PlanetService;
+import kz.zvezdochet.service.SignService;
 
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.jface.viewers.IBaseLabelProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -34,9 +49,7 @@ public class SearchPart extends ModelListView {
 	private CTabFolder folder;
 	
 	@Inject
-	public SearchPart() {
-		
-	}
+	public SearchPart() {}
 
 	@PostConstruct @Override
 	public View create(Composite parent) {
@@ -79,7 +92,7 @@ public class SearchPart extends ModelListView {
 		tab.name = "по имени";
 		tab.image = AbstractUIPlugin.imageDescriptorFromPlugin("kz.zvezdochet.core", "icons/correction_linked_rename.gif").createImage();
 		final Text txSearch = new Text(folder, SWT.BORDER);
-		txSearch.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		txSearch.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 		txSearch.setFocus();
 		txSearch.addListener(SWT.DefaultSelection, new Listener() {
 			@Override
@@ -96,9 +109,49 @@ public class SearchPart extends ModelListView {
 		tab.name = "по знакам Зодиака";
 		tab.image = AbstractUIPlugin.imageDescriptorFromPlugin("kz.zvezdochet", "icons/cosmogram.png").createImage();
 		Group group = new Group(folder, SWT.NONE);
-		//TODO интерфейс
+
+		Label lb = new Label(group, SWT.NONE);
+		lb.setText("Планета");
+		final ComboViewer cvPlanet = new ComboViewer(group, SWT.BORDER | SWT.READ_ONLY);
+		try {
+			cvPlanet.setContentProvider(new ArrayContentProvider());
+			cvPlanet.setLabelProvider(new DictionaryLabelProvider());
+			cvPlanet.setInput(new PlanetService().getList());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		lb = new Label(group, SWT.NONE);
+		lb.setText("Знак Зодиака");
+		final ComboViewer cvSign = new ComboViewer(group, SWT.BORDER | SWT.READ_ONLY);
+		try {
+			cvSign.setContentProvider(new ArrayContentProvider());
+			cvSign.setLabelProvider(new DictionaryLabelProvider());
+			cvSign.setInput(new SignService().getList());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Button bt = new Button(group, SWT.NONE);
+		bt.setText("Искать");
+		bt.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if (cvPlanet.getSelection().isEmpty()
+						|| cvSign.getSelection().isEmpty())
+					return;
+				IStructuredSelection selection = (IStructuredSelection)cvPlanet.getSelection();
+				Planet planet = ((Planet)selection.getFirstElement());
+				selection = (IStructuredSelection)cvSign.getSelection();
+				Sign sign = ((Sign)selection.getFirstElement());
+				findByPlanetSign(planet, sign);
+			}
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {}
+		});
+
 		tab.control = group;
-		GridLayoutFactory.swtDefaults().applyTo(group);
+		GridLayoutFactory.swtDefaults().numColumns(5).applyTo(group);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(group);
 		tabs[1] = tab;
 		
@@ -106,9 +159,49 @@ public class SearchPart extends ModelListView {
 		tab.name = "по домам";
 		tab.image = AbstractUIPlugin.imageDescriptorFromPlugin("kz.zvezdochet", "icons/home.gif").createImage();
 		group = new Group(folder, SWT.NONE);
-		//TODO интерфейс
+
+		lb = new Label(group, SWT.NONE);
+		lb.setText("Планета");
+		final ComboViewer cvPlaneth = new ComboViewer(group, SWT.BORDER | SWT.READ_ONLY);
+		try {
+			cvPlaneth.setContentProvider(new ArrayContentProvider());
+			cvPlaneth.setLabelProvider(new DictionaryLabelProvider());
+			cvPlaneth.setInput(new PlanetService().getList());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		lb = new Label(group, SWT.NONE);
+		lb.setText("Астрологический дом");
+		final ComboViewer cvHouse = new ComboViewer(group, SWT.BORDER | SWT.READ_ONLY);
+		try {
+			cvHouse.setContentProvider(new ArrayContentProvider());
+			cvHouse.setLabelProvider(new DictionaryLabelProvider());
+			cvHouse.setInput(new HouseService().getList());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		bt = new Button(group, SWT.NONE);
+		bt.setText("Искать");
+		bt.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if (cvPlaneth.getSelection().isEmpty()
+						|| cvHouse.getSelection().isEmpty())
+					return;
+				IStructuredSelection selection = (IStructuredSelection)cvPlaneth.getSelection();
+				Planet planet = ((Planet)selection.getFirstElement());
+				selection = (IStructuredSelection)cvHouse.getSelection();
+				House house = ((House)selection.getFirstElement());
+				findByPlanetSign(planet, house);
+			}
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {}
+		});
+
 		tab.control = group;
-		GridLayoutFactory.swtDefaults().applyTo(group);
+		GridLayoutFactory.swtDefaults().numColumns(5).applyTo(group);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(group);
 		tabs[2] = tab;
 
@@ -116,9 +209,64 @@ public class SearchPart extends ModelListView {
 		tab.name = "по аспектам";
 		tab.image = AbstractUIPlugin.imageDescriptorFromPlugin("kz.zvezdochet", "icons/aspect.gif").createImage();
 		group = new Group(folder, SWT.NONE);
-		//TODO интерфейс
+
+		lb = new Label(group, SWT.NONE);
+		lb.setText("Планета");
+		final ComboViewer cvPlaneta = new ComboViewer(group, SWT.BORDER | SWT.READ_ONLY);
+		try {
+			cvPlaneta.setContentProvider(new ArrayContentProvider());
+			cvPlaneta.setLabelProvider(new DictionaryLabelProvider());
+			cvPlaneta.setInput(new PlanetService().getList());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		lb = new Label(group, SWT.NONE);
+		lb.setText("Астрологический аспект");
+		final ComboViewer cvAspect = new ComboViewer(group, SWT.BORDER | SWT.READ_ONLY);
+		try {
+			cvAspect.setContentProvider(new ArrayContentProvider());
+			cvAspect.setLabelProvider(new DictionaryLabelProvider());
+			cvAspect.setInput(new HouseService().getList());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		lb = new Label(group, SWT.NONE);
+		lb.setText("Планета");
+		final ComboViewer cvPlaneta2 = new ComboViewer(group, SWT.BORDER | SWT.READ_ONLY);
+		try {
+			cvPlaneta2.setContentProvider(new ArrayContentProvider());
+			cvPlaneta2.setLabelProvider(new DictionaryLabelProvider());
+			cvPlaneta2.setInput(new PlanetService().getList());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		bt = new Button(group, SWT.NONE);
+		bt.setText("Искать");
+		bt.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if ((cvPlaneta.getSelection().isEmpty()
+						|| cvAspect.getSelection().isEmpty()
+						|| cvPlaneta2.getSelection().isEmpty())
+							|| cvPlaneta.getSelection().equals(cvPlaneta2.getSelection()))
+					return;
+				IStructuredSelection selection = (IStructuredSelection)cvPlaneta.getSelection();
+				Planet planet = ((Planet)selection.getFirstElement());
+				selection = (IStructuredSelection)cvAspect.getSelection();
+				Aspect aspect = ((Aspect)selection.getFirstElement());
+				selection = (IStructuredSelection)cvPlaneta2.getSelection();
+				Planet planet2 = ((Planet)selection.getFirstElement());
+				findByPlanetAspect(planet, planet2, aspect);
+			}
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {}
+		});
+
 		tab.control = group;
-		GridLayoutFactory.swtDefaults().applyTo(group);
+		GridLayoutFactory.swtDefaults().numColumns(6).applyTo(group);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(group);
 		tabs[3] = tab;
 
@@ -140,7 +288,7 @@ public class SearchPart extends ModelListView {
 	 */
 	private void findByName(String text) {
 		try {
-			setData(new EventService().findByName(text));
+			setData(new EventService().findByName(text, -1));
 		} catch (DataAccessException e) {
 			e.printStackTrace();
 		}
@@ -150,7 +298,6 @@ public class SearchPart extends ModelListView {
 	protected String[] initTableColumns() {
 		String[] columns = {
 			"Имя",
-			"Фамилия",
 			"Дата",
 			"Знак Зодиака",
 			"Стихия",
@@ -166,11 +313,10 @@ public class SearchPart extends ModelListView {
 				kz.zvezdochet.bean.Event event = (kz.zvezdochet.bean.Event)element;
 				switch (columnIndex) {
 					case 0: return event.getName();
-					case 1: return event.getSurname();
-					case 2: return DateUtil.formatDateTime(event.getBirth());
-					case 3: return event.getSign();
-					case 4: return event.getElement();
-					case 5: return event.getDescription();
+					case 1: return DateUtil.formatDateTime(event.getBirth());
+					case 2: return event.getSign();
+					case 3: return event.getElement();
+					case 4: return event.getDescription();
 				}
 				return null;
 			}
@@ -180,5 +326,45 @@ public class SearchPart extends ModelListView {
 	@Override
 	public boolean check(int mode) throws Exception {
 		return false;
+	}
+
+	/**
+	 * Поиск по знаку Зодиака планеты
+	 * @param planet планета
+	 * @param sign знак Зодиака
+	 */
+	private void findByPlanetSign(Planet planet, Sign sign) {
+		try {
+			setData(new EventService().findByPlanetSign(planet, sign));
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Поиск по астрологическому дому планеты
+	 * @param planet планета
+	 * @param house астрологический дом
+	 */
+	private void findByPlanetSign(Planet planet, House house) {
+		try {
+			setData(new EventService().findByPlanetHouse(planet, house));
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Поиск по аспекту планет
+	 * @param planet планета
+	 * @param planet2 планета
+	 * @param aspect астрологический аспект
+	 */
+	private void findByPlanetAspect(Planet planet, Planet planet2, Aspect aspect) {
+		try {
+			setData(new EventService().findByPlanetAspect(planet, planet2, aspect));
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+		}
 	}
 }
