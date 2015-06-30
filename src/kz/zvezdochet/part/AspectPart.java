91a3702ce0e884b555b057e12dd9785692f1d51b
nataly <inbox@zvezdochet.guru>
2015-06-30 16:47:53 +0600

    Устранение дубликатов-столбцов Рефакторинг формата таблицы

--- src/kz/zvezdochet/part/AspectPart.java
+++ src/kz/zvezdochet/part/AspectPart.java
@@ -4,17 +4,15 @@ import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import kz.zvezdochet.bean.Planet;
-import kz.zvezdochet.bean.SkyPointAspect;
-import kz.zvezdochet.core.ui.ArrayLabelProvider;
 import kz.zvezdochet.core.ui.view.ListView;
 import kz.zvezdochet.core.ui.view.View;
+import kz.zvezdochet.core.util.CalcUtil;
+import kz.zvezdochet.provider.AspectLabelProvider;
 import kz.zvezdochet.util.Configuration;
 
 import org.eclipse.jface.viewers.IBaseLabelProvider;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.TableColumn;
 
 /**
@@ -26,12 +24,10 @@ public class AspectPart extends ListView {
 	/**
 	 * Конфигурация события
 	 */
-	private Configuration conf;
+	protected Configuration conf;
 
 	@Inject
-	public AspectPart() {
-		
-	}
+	public AspectPart() {}
 	
 	@PostConstruct @Override
 	public View create(Composite parent) {
@@ -49,43 +45,22 @@ public class AspectPart extends ListView {
 
 	@Override
 	protected void addColumns() {
+		removeColumns();
 		if (conf != null) {
-			table.removeAll();
 			TableColumn tableColumn = new TableColumn(table, SWT.NONE);
 			for (int i = 0; i < conf.getPlanets().size(); i++) {
 				Planet planet = (Planet)conf.getPlanets().get(i);
 				tableColumn = new TableColumn(table, SWT.NONE);
-				tableColumn.setText(planet.getName());		
+				tableColumn.setText(CalcUtil.roundTo(planet.getCoord(), 1) + "");
+				tableColumn.setImage(planet.getImage());
+				tableColumn.setToolTipText(planet.getName());
 			}
 		}
 	}
 
 	@Override
 	protected IBaseLabelProvider getLabelProvider() {
-		return new ArrayLabelProvider() {
-			@Override
-			public String getColumnText(Object element, int columnIndex) {
-				Object[] array = (Object[])element;
-				Object val = array[columnIndex];
-				if (null == val)
-					return null;
-				return (val instanceof SkyPointAspect)
-					? String.valueOf(((SkyPointAspect)val).getScore())
-					: val.toString();
-			}
-			@Override
-			public Color getBackground(Object element, int columnIndex) {
-				Object[] array = (Object[])element;
-				Object val = array[columnIndex];
-				if (val instanceof SkyPointAspect) {
-					SkyPointAspect aspect = (SkyPointAspect)val;
-					if (aspect.getAspect() != null)
-						return aspect.getAspect().getType().getDimColor();
-				}
-				int color = (null == val) ? SWT.COLOR_BLACK : SWT.COLOR_WHITE;
-				return  Display.getDefault().getSystemColor(color);
-			}
-		};
+		return new AspectLabelProvider();
 	}
 
 	@Override
---
+++ src/kz/zvezdochet/provider/AspectLabelProvider.java
@@ -0,0 +1,37 @@
+package kz.zvezdochet.provider;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
+
+import kz.zvezdochet.bean.SkyPointAspect;
+import kz.zvezdochet.core.ui.ArrayLabelProvider;
+
+/**
+ * Формат таблицы аспектов
+ * @author Nataly Didenko
+ */
+public class AspectLabelProvider extends ArrayLabelProvider {
+	@Override
+	public String getColumnText(Object element, int columnIndex) {
+		Object[] array = (Object[])element;
+		Object val = array[columnIndex];
+		if (null == val)
+			return null;
+		return (val instanceof SkyPointAspect)
+			? String.valueOf(((SkyPointAspect)val).getScore())
+			: val.toString();
+	}
+	@Override
+	public Color getBackground(Object element, int columnIndex) {
+		Object[] array = (Object[])element;
+		Object val = array[columnIndex];
+		if (val instanceof SkyPointAspect) {
+			SkyPointAspect aspect = (SkyPointAspect)val;
+			if (aspect.getAspect() != null)
+				return aspect.getAspect().getType().getDimColor();
+		}
+		int color = (null == val) ? SWT.COLOR_BLACK : SWT.COLOR_WHITE;
+		return  Display.getDefault().getSystemColor(color);
+	}
+}
