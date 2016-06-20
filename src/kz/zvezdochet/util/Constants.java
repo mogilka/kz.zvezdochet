ad71e18f04b381876f87bd84b6de4a788de7fe66
Nataly Didenko <inbox@zvezdochet.guru>
2016-06-20 20:01:31 +0600

    Оптимизация поиска

--- src/kz/zvezdochet/util/Constants.java
+++ src/kz/zvezdochet/util/Constants.java
@@ -8,5 +8,5 @@ package kz.zvezdochet.util;
 public class Constants {
 	public static final String PREF_RECENT = "recent";
 	public static final String PREF_RECENT_EVENT = "recent_event";
-	public static final int PREF_RECENT_MAX = 30;
+	public static final int PREF_RECENT_MAX = 10;
 }
