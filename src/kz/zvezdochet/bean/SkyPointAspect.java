3c8d5f9c37208d3a04fc56963a57979a708ba041
Nataly Didenko <inbox@zvezdochet.guru>
2019-03-24 19:23:53 +0600

    Доработка для транзитов

--- src/kz/zvezdochet/bean/SkyPointAspect.java
+++ src/kz/zvezdochet/bean/SkyPointAspect.java
@@ -168,4 +168,17 @@ public class SkyPointAspect {
 		exact = spa.exact;
 		application = spa.application;
 	}
+
+	/**
+	 * Описание
+	 */
+	private String descr;
+
+	public String getDescr() {
+		return descr;
+	}
+
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
 }
