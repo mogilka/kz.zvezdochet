6c1c3784a16632923e90fc11cd4261c0fbf713c8
natalie <natalie@natalie-Inspiron-3847>
2022-12-02 20:25:14 +0600

    Не сохраняем импорт в файл

--- src/kz/zvezdochet/handler/ImportRunHandler.java
+++ src/kz/zvezdochet/handler/ImportRunHandler.java
@@ -1,8 +1,5 @@
 package kz.zvezdochet.handler;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
 import java.util.Date;
 import java.util.List;
 
@@ -12,12 +9,10 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 
-import kz.zvezdochet.Activator;
 import kz.zvezdochet.bean.Event;
 import kz.zvezdochet.core.handler.Handler;
 import kz.zvezdochet.core.ui.util.DialogUtil;
 import kz.zvezdochet.core.util.DateUtil;
-import kz.zvezdochet.core.util.PlatformUtil;
 import kz.zvezdochet.part.ImportPart;
 
 public class ImportRunHandler extends Handler {
@@ -45,17 +40,8 @@ public class ImportRunHandler extends Handler {
 				log.append("Новый добавлен: " + event.getId() + " " + event.toLog() + "\n");
 				++imported;
 			}
-			//логируем
-			log.append("Добавлено: " + imported + "\t");
-
-			String datafile = PlatformUtil.getPath(Activator.PLUGIN_ID, "/out/import.log").getPath(); //$NON-NLS-1$
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
-				new FileOutputStream(datafile, true), "UTF-8"));
-			writer.append(log);
-			writer.close();
 			//TODO показывать диалог, что документ сформирован
-			//а ещё лучше открывать его
-			System.out.println("Импорт завершён");
+			System.out.println("Импорт завершён " + imported);
 			updateStatus("Импорт завершён", false);
 			DialogUtil.alertInfo("Импорт завершён");
 		} catch (Exception e) {
